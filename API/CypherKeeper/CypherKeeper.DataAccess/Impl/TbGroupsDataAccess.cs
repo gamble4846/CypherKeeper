8af5a8fe71d268389403d11efdde18c340351827
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-05-26 18:35:48 +0530

    asd

--- API/CypherKeeper/CypherKeeper.DataAccess/Impl/TbGroupsDataAccess.cs
+++ API/CypherKeeper/CypherKeeper.DataAccess/Impl/TbGroupsDataAccess.cs
@@ -1,4 +1,10 @@
-﻿using CypherKeeper.DataAccess.Interface;
+﻿using AuthLayer.Utility;
+using CypherKeeper.DataAccess.Interface;
+using CypherKeeper.Model;
+using EasyCrudLibrary.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +15,21 @@ namespace CypherKeeper.DataAccess.Impl
 {
     public class TbGroupsDataAccess : ITbGroupsDataAccess
     {
+        private CommonFunctions _cf { get; set; }
+        private string ConnectionString { get; set; }
+        public TbGroupsDataAccess(IConfiguration configuration, IHttpContextAccessor httpContextAccessor, IHostingEnvironment env)
+        {
+            try
+            {
+                _cf = new CommonFunctions(configuration, env.ContentRootPath, httpContextAccessor);
+                ConnectionString = _cf.GetNewConnectionString();
+            }
+            catch (Exception) { }
+        }
+
+        public List<tbGroupsModel> Get(int page = 1, int itemsPerPage = 100, List<OrderByModel> orderBy = null)
+        {
+
+        }
     }
 }
--- API/CypherKeeper/CypherKeeper.DataAccess/Interface/ITbGroupsDataAccess.cs
+++ API/CypherKeeper/CypherKeeper.DataAccess/Interface/ITbGroupsDataAccess.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CypherKeeper.Model;
+using EasyCrudLibrary.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace CypherKeeper.DataAccess.Interface
 {
     public interface ITbGroupsDataAccess
     {
+        public List<tbGroupsModel> Get(int page = 1, int itemsPerPage = 100, List<OrderByModel> orderBy = null);
     }
 }
