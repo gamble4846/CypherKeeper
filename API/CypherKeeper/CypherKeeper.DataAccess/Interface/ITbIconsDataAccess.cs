25ba23a2da73c1b6623beb52bd3152892456643b
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-05-27 11:12:07 +0530

    TbIconsDataAccess Done

---
+++ API/CypherKeeper/CypherKeeper.DataAccess/Interface/ITbIconsDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using CypherKeeper.Model;
+using EasyCrudLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CypherKeeper.DataAccess.Interface
+{
+    public interface ITbIconsDataAccess
+    {
+        List<tbIconsModel> Get(int page = 1, int itemsPerPage = 100, List<OrderByModel> orderBy = null, bool onlyNonDeleted = true);
+        tbIconsModel GetById(Guid Id, bool onlyNonDeleted = true);
+        tbIconsModel Add(tbIconsModel model);
+        bool Update(Guid Id, tbIconsModel model);
+        bool Delete(Guid Id);
+        bool Restore(Guid Id);
+    }
+}
